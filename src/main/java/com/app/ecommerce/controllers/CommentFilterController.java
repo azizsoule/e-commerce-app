f5502f2d3581d34b8db04b135ed01a8d860a8565
usermariam <ozmouatt@gmail.com>
2021-09-07 09:50:53 +0000

    Dictionnary and order

--- src/main/java/com/app/ecommerce/controllers/CommentFilterController.java
+++ src/main/java/com/app/ecommerce/controllers/CommentFilterController.java
@@ -1,15 +1,69 @@
 package com.app.ecommerce.controllers;
 
+import com.app.ecommerce.dtos.CatalogDTO;
+import com.app.ecommerce.dtos.DictionaryDTO;
+import com.app.ecommerce.services.CatalogService;
+import com.app.ecommerce.services.DictionaryService;
+import com.app.ecommerce.utils.Router;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class CommentFilterController {
-    private static final String URI="/comment-filter";
-    private static final String VIEW="dictionnary";
+    private static final String URI="/comment_filter";
+    private static final String ADD_URI="/add-dictionary";
+    private static final String LIST_VIEW="dictionnary";
+    private static final String VIEW="add_dictionnary";
+    private static final String EDIT_VIEW="edit_dictionnary";
+
+    @Autowired
+    private DictionaryService dictionaryService;
 
     @GetMapping(URI)
-    private String getWord(){
+    private String getWords(Model model){
+        model.addAttribute("comment_filter",dictionaryService.findAll());
+            return LIST_VIEW;
+    }
+
+    @GetMapping(ADD_URI)
+    private String Word(Model model){
+        model.addAttribute("dictionaryDTO",new DictionaryDTO());
         return VIEW;
     }
+
+    @GetMapping(URI + "/{id}")
+    private String getWordById(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("dictionaryDTO", dictionaryService.findById(id));
+        return EDIT_VIEW;
+    }
+
+    @PostMapping(ADD_URI + "/save")
+    private String postWord(DictionaryDTO dictionaryDTO, RedirectAttributes ra) {
+        try {
+            dictionaryService.save(dictionaryDTO);
+            ra.addFlashAttribute("success", "Successfully saved !");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            ra.addFlashAttribute("fail", "Registration failure !");
+        }
+        return Router.redirectTo(ADD_URI);
+    }
+
+   @PostMapping(URI + "/{id}/delete")
+    private String deleteCatalog(@PathVariable("id") Long id, DictionaryDTO dictionaryDTO, RedirectAttributes ra) {
+        try {
+            dictionaryService.delete(dictionaryDTO);
+            ra.addFlashAttribute("success", "Successfully deleted !");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            ra.addFlashAttribute("fail", "Fail to delete !");
+        }
+        return Router.redirectTo(URI);
+    }
+
 }
--- src/main/java/com/app/ecommerce/controllers/OrderController.java
+++ src/main/java/com/app/ecommerce/controllers/OrderController.java
@@ -1,15 +1,94 @@
 package com.app.ecommerce.controllers;
 
+import com.app.ecommerce.dtos.CatalogDTO;
+import com.app.ecommerce.services.CustomerService;
+import com.app.ecommerce.services.PaymentDetailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.app.ecommerce.dtos.OrderDetailDTO;
+import com.app.ecommerce.services.OrderService;
+import com.app.ecommerce.utils.Router;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 public class OrderController {
-    private static final String URI="/orders";
-    private static final String VIEW="orders";
+    private static final String URI = "/orders";
+    private static final String ADD_URI = "/add-order";
+    private static final String LIST_VIEW = "orders";
+    private static final String LIST_CUSTOMMER = "customer";
+    private static final String VIEW = "add_order";
+    private static final String EDIT_VIEW = "edit_order";
+
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private PaymentDetailService paymentDetailService;
+
 
     @GetMapping(URI)
-    private String getOrders(){
+    private String getOrders(Model model) {
+        model.addAttribute("orders",customerService.findAll());
+        return LIST_VIEW;
+
+    }
+
+    @GetMapping(ADD_URI)
+    private String getOrder(Model model){
+        model.addAttribute("orderDetailDTO",new OrderDetailDTO());
         return VIEW;
+    }
+
+    @GetMapping(URI + "/{id}")
+    private String getOrderById(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("orderDetailDTO", orderService.findById(id));
+        return EDIT_VIEW;
+    }
+
+     @PostMapping(ADD_URI + "/save")
+    private String postOrder(OrderDetailDTO orderDetailDTO, RedirectAttributes ra) {
+        try {
+            orderService.save(orderDetailDTO);
+            ra.addFlashAttribute("success", "Successfully saved !");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            ra.addFlashAttribute("fail", "Registration failure !");
+        }
+        return Router.redirectTo(ADD_URI);
+    }
+
+
+    @PostMapping(URI + "/{id}/update")
+    private String updateOrder(@PathVariable("id") Long id, OrderDetailDTO orderDetailDTO,
+                                 RedirectAttributes ra) {
+        try {
+            orderService.update(orderDetailDTO);
+            ra.addAttribute("id", id).addFlashAttribute("success", "Successfully updated !");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            ra.addAttribute("id", id).addFlashAttribute("fail", "Update failure !");
+        }
+        return Router.redirectTo(URI + "/{id}");
+    }
+
+    @PostMapping(URI + "/{id}/delete")
+    private String deleteOrder(@PathVariable("id") Long id, OrderDetailDTO orderDetailDTO, RedirectAttributes ra) {
+        try {
+            orderService.delete(orderDetailDTO);
+            ra.addFlashAttribute("success", "Successfully deleted !");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            ra.addFlashAttribute("fail", "Fail to delete !");
+        }
+        return Router.redirectTo(URI);
     }
 }
