20a12f68798871d842dc6dd437377ccc72fa94af
Abrahamkone <62753365+Abrahamkone@users.noreply.github.com>
2021-09-08 19:23:21 +0000

    sub cat

--- src/main/java/com/app/ecommerce/repositories/AddressRepository.java
+++ src/main/java/com/app/ecommerce/repositories/AddressRepository.java
@@ -3,5 +3,8 @@ package com.app.ecommerce.repositories;
 import com.app.ecommerce.models.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AddressRepository extends JpaRepository<Address, Long> {
+
 }
