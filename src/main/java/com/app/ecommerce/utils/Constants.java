d55a8bb2da22ef40d64a8f32aa07bba85fbf32c5
Aziz Soulé <aziz.soule.dev@gmail.com>
2021-09-16 09:31:21 +0000

    100%

--- src/main/java/com/app/ecommerce/utils/Constants.java
+++ src/main/java/com/app/ecommerce/utils/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static int ENCODER_LENGTH = 10;
 
-    public static String MEDIA_SERVER_IP = "192.168.1.9";
+    public static String MEDIA_SERVER_IP = "192.168.43.224";
 
     public static String MEDIA_SERVER_PORT = "8080";
 
